17561c4a3e7e6e7575147d5f01f7feebd732469a
Sheva Satrian <sheva.sepot@gmail.com>
2023-12-23 09:46:26 +0700

    tugas hari-7

--- components/header/index.js
+++ components/header/index.js
@@ -1,11 +1,18 @@
 import Menu from "../menu";
 import { withAuth } from "../with-auth";
 import styles from './styles.module.css';
+import Link from "next/link";
 
 function Header() {
   return (
     <div className={styles.header}>
-      <Menu />
+      {/* <Menu /> */}
+      <ul>
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/profile">Profile</Link></li>
+        <li><Link href="/users">Users</Link></li>
+        <li><Link href="/users/detail">Users Detail</Link></li>
+      </ul>
     </div>
   );
 }
