97ebd38ea877d207d0bf90ea871916378163f590
Sheva Satrian <sheva.sepot@gmail.com>
2023-12-24 11:25:17 +0700

    tugas hari-9

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 
 import { Inter } from 'next/font/google'
 import Layout from '@/layout'
+import { useEffect } from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Main({ children }) {
+
+  useEffect(() => {
+
+    fetch("/api/hello")
+    .then((res) => res.json())
+    .then((res) => console.log("response => ", res))
+    .catch((err) =>console.log("err => ", err))
+
+  }, [])
   return (
     <>
       <Layout metaTitle="Home">
